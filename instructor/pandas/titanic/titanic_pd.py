31aa0384a3b4f8dcc9fd0a6d5b3e7cfc70a416f3
cetec-bchavez <98634131+cetec-bchavez@users.noreply.github.com>
2023-07-24 17:37:12 -0500

    a

--- instructor/pandas/titanic/titanic_pd.py
+++ instructor/pandas/titanic/titanic_pd.py
@@ -3,13 +3,35 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 titanic = None
+sales = None
 
 def cargarArchivo():
-    global titanic
+    global titanic,sales
+    
+    sales = pd.read_csv('C:/data/use_sales_data.csv')
+    #titanic = pd.read_csv('C:/data/titanic.csv')
 
-    titanic = pd.read_csv('C:/data/titanic.csv')
+    #titanic = pd.read_csv('C:/data/titanic.csv')
     #titanic = pd.read_excel('C:/data/titanic.xlsx', sheet_name='Titanic_train')
+    #print(titanic[['Name']])
+    #print(titanic)
 
-    print(titanic)
+    #print(sales)
+
+def cargarInfoGeneral():
+    global titanic,sales
+
+    print(sales.info())
+    #print(titanic.corr())
+
+# select Name,Age from titanic
+def mostrarNombreEdad():
+    global titanic,sales
+
+    print(sales['Country'])
 
 cargarArchivo()
+
+cargarInfoGeneral()
+
+mostrarNombreEdad()
